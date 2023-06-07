152ea4b066a75217556d84efb026836a5e980f53
Jose Rivera <jose.rivera@cornershopapp.com>
2023-06-07 12:48:45 -0600

    leet-code design problem added

---
+++ CodeForces/golang/help_far_away_kingdom.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func roundNumber(num string) string {
+	parts := strings.Split(num, ".")
+	intPart := parts[0]
+	decPart := parts[1]
+
+	lastDigit := int(intPart[len(intPart)-1] - '0')
+	if lastDigit != 9 {
+		fracDigit := int(decPart[0] - '0')
+		if fracDigit < 5 {
+			return intPart
+		}
+		return intPart[:len(intPart)-1] + string(lastDigit+'1')
+	}
+
+	return "GOTO Vasilisa."
+
+}
+
+func main() {
+	var number string
+	fmt.Scanln(&number)
+
+	fmt.Println(roundNumber(number))
+	fmt.Println('0')
+	fmt.Println('1')
+}
